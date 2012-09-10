4935f64831502027324c6c449e96c1f33b6e14b0
Doug Hellmann <doug.hellmann@gmail.com>
2012-09-09 20:50:22 -0400

    add gunicorn module for demonstration
    Signed-off-by: Doug Hellmann <doug.hellmann@gmail.com>

---
+++ bartender/gunicorn.py
@@ -0,0 +1,16 @@
+"""Load a PathDispatcher using settings from the environment, useful with gunicorn.
+
+The namespace is taken from ``BARTENDER_NAMESPACE`` with a default
+of "bartender".
+"""
+
+import logging
+import os
+
+from bartender.path import PathDispatcher
+
+LOG = logging.getLogger(__name__)
+
+namespace = os.environ.get('BARTENDER_NAMESPACE', 'bartender')
+LOG.info('Creating dispatcher with namespace %r', namespace)
+app = PathDispatcher(namespace)
